7c654264bb54688c013734734bbe811e46727c94
RyanMickey <rmickey42@gmail.com>
2018-09-08 11:39:03 -0400

    added bestiary info

--- commands/compendium/bestiary.js
+++ commands/compendium/bestiary.js
@@ -24,14 +24,51 @@ class bestiaryCmd extends commando.Command {
             for (var i = 0; i < listObj.length; i++) {
                 if (listObj[i].name.toUpperCase() == args.toUpperCase().trim()) {
                     // this is the monster the player called upon
+                    message.channel.send("__**General**__");
+                    message.channel.send("Name: " + listObj[i].name);
+                    message.channel.send("Size: " + listObj[i].size);
+                    message.channel.send("Type: " + listObj[i].type);
+                    message.channel.send("Alignment: " + listObj[i].alignment);
+                    message.channel.send("Armor Class: " + listObj[i].armor_class);
                     message.channel.send("Hit Points: " + listObj[i].hit_points);
+
+                    message.channel.send("__**Special Abilities**__");
+                    for(var j = 0; j < listObj[i].special_abilities.length; j++){
+                      message.channel.send(listObj[i].special_abilities[j].name + ": " + listObj[i].special_abilities[j].desc);
+                    }
+
+                    message.channel.send("__**Actions**__");
+                    for(var j = 0; j < listObj[i].actions.length; j++){
+                      message.channel.send(listObj[i].actions[j].name + ": " + listObj[i].actions[j].desc);
+                    }
+
+                    if(listObj[i].legendary_actions !== null){
+                      message.channel.send("__**Legendary Abilities**__");
+                      for(var j = 0; j < listObj[i].legendary_actions.length; j++){
+                        message.channel.send(listObj[i].legendary_actions[j].name + ": " + listObj[i].legendary_actions[j].desc);
+                      }
+                    }
+
+                    message.channel.send("__**Stats**__");
+                    message.channel.send("Speed: " + listObj[i].speed);
+                    message.channel.send("Strength: " + listObj[i].strength);
+                    message.channel.send("Dexterity: " + listObj[i].dexterity);
+                    message.channel.send("Constitution: " + listObj[i].constitution);
+                    message.channel.send("Intelligence: " + listObj[i].intelligence);
+                    message.channel.send("Wisdom: " + listObj[i].wisdom);
+                    message.channel.send("Charisma: " + listObj[i].charisma);
+
+                    message.channel.send("__**Other**__");
+                    message.channel.send("Senses: " + listObj[i].senses);
+                    message.channel.send("Languages: " + listObj[i].languages);
+                    message.channel.send("Challenge Rating: " + listObj[i].challenge_rating);
                     return;
                 }
             }
             message.channel.send("Entry not found.");
         }
         else {
-            message.channel.send('You do not have the authority to view the archives of the Dungeon Master');
+            message.channel.send('You do not have the authority to view the archives of the Dungeon Master.');
         }
     }
 }
