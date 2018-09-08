5c47bedfa6e824417cfbb6f8c3ceddf035b611f3
RyanMickey <rmickey42@gmail.com>
2018-09-08 00:46:02 -0400

    Started json parsing

--- commands/compendium/bestiary.js
+++ commands/compendium/bestiary.js
@@ -1,6 +1,9 @@
 const commando = require('discord.js-commando');
+const fs = require("fs");
 
 class bestiaryCmd extends commando.Command {
+    var content;
+
     constructor(client) {
         super(client, {
             name: 'bestiary',
@@ -8,10 +11,20 @@ class bestiaryCmd extends commando.Command {
             memberName: 'bestiary',
             description: 'Gets the information of a monster from the bestiary.'
         });
+        content = fs.readFileSync("monsters.txt");
     }
 
     async run(message, args) {
-
+      message.channel.send("Searching the archives...");
+      let listObj = JSON.parse(content);
+      if(args.trim() !== null) return;
+      for(var i = 0; i < listObj.length; i++){
+        if(listObj[i].name.toUpperCase() == args.toUpperCase()){
+          message.channel.send("Hit Points: " + listObj[i].hit_points)
+          return;
+        }
+      }
+      message.channel.send("Entry not found.");
     }
 }
 
