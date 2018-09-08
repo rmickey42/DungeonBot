868c3e027f6cd8b13c64798c9ef0348b86001609
RyanMickey <rmickey42@gmail.com>
2018-09-08 11:12:56 -0400

    fixed bestiary reading

--- commands/compendium/bestiary.js
+++ commands/compendium/bestiary.js
@@ -2,6 +2,7 @@ const commando = require('discord.js-commando');
 const fs = require("fs");
 
 var content;
+var listObj
 
 class bestiaryCmd extends commando.Command {
     constructor(client) {
@@ -12,12 +13,13 @@ class bestiaryCmd extends commando.Command {
             description: 'Gets the information of a monster from the bestiary.'
         });
         content = fs.readFileSync(__dirname + "/monsters.txt");
+        listObj = JSON.parse(content);
+        console.log("Parsed bestiary.");
     }
 
     async run(message, args) {
       message.channel.send("Searching the archives...");
-      let listObj = JSON.parse(content);
-      if(args.trim() !== null) return;
+      if(args.trim() == null) return;
       console.log("Hit Points: " + listObj);
       for(var i = 0; i < listObj.length; i++){
         if(listObj[i].name.toUpperCase() == args.toUpperCase().trim()){
