02296606d553577e36402aede71a602b04e5a938
DESKTOP-NS65CE1\Christopher Roche <ctroche411@gmail.com>
2018-10-05 22:21:49 -0400

    new regenerated token (thanks for that hpfxd please spare us in future)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Commando = require('discord.js-commando');
 const bot = new Commando.Client({ commandPrefix: '~' });
-const TOKEN = 'NDg0NDgxMTQ3Mzg5MzQ1Nzky.DnMmAw.JtIeHjS0O7knam5EWwFxp2T3vFI';
+const TOKEN = 'NDg0NDgxMTQ3Mzg5MzQ1Nzky.Dpmr1w._9wixrkpp5R3Q8zccfZbnM6NvHc';
 
 bot.registry.registerGroup('dice', 'Dice Rolls');
 bot.registry.registerGroup('character_creation', 'Character Creation');
